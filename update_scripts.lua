3a80483d391cf7c5e9e343c34caa0feb3366e558
Michael W. Johnson <digitlworld@gmail.com>
2023-02-11 13:33:02 -0500

    Continuing to build bootstrapiness

--- update_scripts.lua
+++ update_scripts.lua
@@ -1,6 +1,19 @@
 -- Root script for getting latest version of scripts
-local gServer = "raw.githubusercontent.com"
+local gRawServer = "raw.githubusercontent.com"
+
 local gUser = "DigitlWorld"
 local gRepo = "cc_scripts"
 local gRef = "master"
 
+-- Download File
+function downloadFile( path, url, name )
+    writeCenter("Downloading File: "..name)
+    dirPath = path:gmatch('([%w%_%.% %-%+%,%;%:%*%#%=%/]+)/'..name..'$')()
+    if dirPath ~= nil and not fs.isDir(dirPath) then fs.makeDir(dirPath) end
+    local content = http.get(url)
+    local file = fs.open(path,"w")
+    file.write(content.readAll())
+    file.close()
+end
+
+downloadFile( ., "https://" .. gRawServer .. "/" .. gUser .. "/" .. gRepo .. "/github_download.lua", "github_download.lua" )
