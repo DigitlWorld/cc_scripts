fcc83a455354522cb20acab28ab76946009a6d69
Michael W. Johnson <digitlworld@gmail.com>
2023-02-11 14:10:09 -0500

    Agh, stupid github raw cache! 🙄

--- update.lua
+++ update.lua
@@ -1,4 +1,10 @@
 -- Root script for getting latest version of the rest of the scripts
+local gDesiredBranch = ...
+
+if gDesiredBranch == nil then
+    gDesiredBranch = "master"
+end
+
 if not fs.exists( "/github_download.lua" ) then
     shell.execute( "wget", "https://raw.githubusercontent.com/DigitlWorld/cc_scripts/master/github_download.lua", "/github_download.lua")
 end
@@ -7,7 +13,7 @@ if fs.exists( "/cc_scripts") then
     fs.delete( "/cc_scripts" )
 end
 
-shell.execute( "/github_download.lua", "DigitlWorld", "cc_scripts", ".", ".", "master")
+shell.execute( "/github_download.lua", "DigitlWorld", "cc_scripts", ".", ".", gDesiredBranch)
 
 if fs.exists("/cc_scripts/update.lua") then
     fs.delete( "/update.lua" )
