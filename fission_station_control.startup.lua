ae41d36f25eb150c6a5dade77408ad72d711b76c
Michael W. Johnson <digitlworld@gmail.com>
2023-02-12 23:56:02 -0500

    caught up the template

--- fission_station_control.startup.lua
+++ fission_station_control.startup.lua
@@ -6,7 +6,9 @@ local reactor = peripheral.wrap("<reactor name>")
 local boiler = peripheral.wrap("<boiler name>")
 local turbine = peripheral.wrap("<turbine name>")
 local monitor = peripheral.wrap("<monitor name>")
+local indMatrix = peripheral.wrap("<matrix name>")
+local heater = peripheral.wrap("<heater name>")
 
-local controller = FissionStationControl.new(reactor, boiler, turbine, monitor)
+local controller = FissionStationControl.new(reactor, boiler, turbine, indMatrix, heater, monitor)
 
 controller:run()
