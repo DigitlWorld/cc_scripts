385a26b8d551c1ff0121b2e9167f2e4164af5734
Michael W. Johnson <digitlworld@gmail.com>
2023-02-19 20:14:16 -0500

    Safer startup script + auto-discovery of peripherals

--- fission_station_control.startup.lua
+++ fission_station_control.startup.lua
@@ -2,13 +2,42 @@ package.path = "/cc_scripts/?.lua;" .. package.path
 
 local FissionStationControl = require("FissionStationControl")
 
-local reactor = peripheral.wrap("<reactor name>")
-local boiler = peripheral.wrap("<boiler name>")
-local turbine = peripheral.wrap("<turbine name>")
-local monitor = peripheral.wrap("<monitor name>")
-local indMatrix = peripheral.wrap("<matrix name>")
-local heater = peripheral.wrap("<heater name>")
+local gStarting = true
+local gRestart = false
 
-local controller = FissionStationControl.new(reactor, boiler, turbine, indMatrix, heater, monitor)
+while gStarting do
 
-controller:run()
+    local reactor = peripheral.find("fissionReactorLogicAdapter")
+
+    -- Safety shutdown. If this program is starting and crashes
+    -- because  other components weren't available (like after a server restart 
+    -- with partially loaded chunks), then the reactor isn't going to be 
+    -- monitored and should be shutdown until monitoring starts.
+    if reactor ~= nil and reactor.getStatus() then
+        gRestart = true
+        reactor.scram()
+    end
+
+    local boiler = peripheral.wrap("boilerValve")
+    local turbine = peripheral.wrap("turbineValve")
+    local monitor = peripheral.wrap("monitor")
+
+    local indMatrix = peripheral.wrap("inductionPort")
+    local heater = peripheral.wrap("resistiveHeater")
+
+    -- If all of the parts are available, start up the main loop.
+    if reactor ~= nil and boiler ~= nil and turbine ~= nil and indMatrix ~= nil and heater ~= nil then
+        gStarting = false
+
+        local controller = FissionStationControl.new(reactor, boiler, turbine, indMatrix, heater, monitor)
+
+        -- If we shutdown the reactor when we started, restart it here once
+        -- the monitoring system is likely to start.
+        if gRestart then
+            reactor.activate()
+        end
+
+        controller:run()
+    end
+    sleep(0.1)
+end
